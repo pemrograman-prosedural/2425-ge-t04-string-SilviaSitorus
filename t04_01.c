bba341428e801753c94d9bcc12a706649a0c3bd1
SilviaSitorus <silviaeklesiana12@gmail.com>
2025-02-27 14:41:10 +0700

    Pair-28

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,16 @@
-// NIM - Name
-// NIM - Name
+// 12S24002 - Petra Ignatius Pengayoman Naibaho
+// 12S24004 - Silvia Eklesiana Sitorus
 
 #include <stdio.h>
-
-int main(int _argv, char **_argc) {
-  // codes
-
-  return 0;
+int main() {
+    char str[61];
+    scanf("%60s", str);
+    
+    for (int i = 0; str[i] != '\0'; i += 3) {
+        int ascii_value = ((str[i] - '0') * 100 + (str[i+1] - '0') * 10 + (str[i+2] - '0'));
+        printf("%c", ascii_value);
+    }
+    
+    printf("\n");
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,17 @@
-// NIM - Name
-// NIM - Name
+// 12S24002 - Petra Ignatius Pengayoman Naibaho
+// 12S24004 - Silvia Eklesiana Sitorus
 
 #include <stdio.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main() {
+  char input[21];
+  fgets(input, sizeof(input), stdin);
+
+  for (int i = 0; input[i] != '\0' && input[i] != '\n'; i++) {
+    printf("%03d", input[i]);
+  }
+  printf("013\n");
 
   return 0;
+
 }
